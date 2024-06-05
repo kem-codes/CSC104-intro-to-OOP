7e3ca4bcbc6837e24697a46210d9e657381e019a
kem-codes <150168585+kem-codes@users.noreply.github.com>
2024-06-05 18:32:52 +0100

    Add files via upload

---
+++ user.py
@@ -0,0 +1,19 @@
+class User: 
+    def _init_(self, schoolName, schoolAddress):
+        self.schoolName= schoolName
+        self.schoolAddress=schoolAddress
+    def show(self):
+        print(self.schoolName, "\nAdress", self.schoolAddress)
+     
+    Kemi= User("BIngham university", "Nassarawa")
+    def register(self, name, phone, email, password):
+        return name
+    def login(self, email, password):
+        if email == "test@mail.com" and password == "abc":
+            return True
+        else:
+            return False
+
+
+
+
